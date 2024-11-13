3f064e9ba72b8ffd8cf5888780932f8192c79e86
VesTheCoder <vladislav.blackrock@gmail.com>
2024-11-13 03:35:03 +0000

    v1, ready to deploy

--- pet_shop/settings.py
+++ pet_shop/settings.py
@@ -84,18 +84,18 @@ WSGI_APPLICATION = 'pet_shop.wsgi.application'
 
 DATABASES = {}
 if os.getenv('DB_ENV') == 'dev':
-    DATABASES = {
-        'default': {
+    DATABASES['default'] = {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
-    }
     }
 elif os.getenv('DB_ENV') == 'prod':
-    DATABASES = {
-        'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    DATABASES['default'] = {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('DATABASE_NAME'),
+        'USER': os.getenv('DATABASE_USER'),
+        'PASSWORD': os.getenv('DATABASE_PASSWORD'),
+        'HOST': os.getenv('DATABASE_HOST'),
+        'PORT': os.getenv('DATABASE_PORT'),
     }
 else:
     raise ValueError('DB_ENV is not set')
